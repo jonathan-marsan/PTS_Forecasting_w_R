23c6afde4d29c1a1a90dc8b0016f356af6b741f7
jonathan000 <jonathan.marsan@unbounce.com>
2016-05-22 17:47:19 -0400

    Check prediction accurary of 2nd order polynomial trend.

--- Amtrak Fig 3-1.R
+++ Amtrak Fig 3-1.R
@@ -1,8 +1,8 @@
 library(forecast)
 
 Amtrak.data <- read.csv("Amtrak data.csv")
-ridership.ts <- ts(Amtrak.data$Ridership, start = c(1991, 1), end = c(2004, 3), freq = 12)
-
+ridership.ts <- ts(Amtrak.data$Ridership,
+                   start = c(1991, 1), end = c(2004, 3), freq = 12)
 # Figure 3-1
 plot(ridership.ts, ylim = c(1300, 2600),  ylab = "Ridership", xlab = "Time", bty = "l", xaxt = "n", xlim = c(1991,2006.25))
 axis(1, at = seq(1991, 2006, 1), labels = format(seq(1991, 2006, 1), digits = 2))
@@ -11,6 +11,8 @@ lines(c(2004.25, 2004.25), c(0, 3500))
 text(1996.25, 2500, "Training")
 text(2002.75, 2500, "Validation")
 text(2005.25, 2500, "Future")
-arrows(2004 - 3,2450,1991.25,2450,code=3,length=0.1,lwd=1,angle=30)
-arrows(2004.5 - 3,2450,2004,2450,code=3,length=0.1,lwd=1,angle=30)
-arrows(2004.5,2450,2006,2450,code=3,length=0.1,lwd=1,angle=30)
+arrows(2004 - 3,2400,1991.25,2400,code=3,length=0.1,lwd=1,angle=30)
+arrows(2004.5 - 3,2400,2004,2400,code=3,length=0.1,lwd=1,angle=30)
+arrows(2004.5,2400,2006,2400,code=3,length=0.1,lwd=1,angle=30)
+
+
--- Amtrak Fig 3-2.R
+++ Amtrak Fig 3-2.R
@@ -3,10 +3,12 @@ Amtrak.data <- read.csv("Amtrak data.csv")
 ridership.ts <- ts(Amtrak.data$Ridership, start = c(1991, 1), end = c(2004, 3), freq = 12)
 
 stepsAhead <- 36
+# Define training period
 nTrain <- length(ridership.ts) - stepsAhead
+# Use window() to create training subset
 train.ts <- window(ridership.ts, start = c(1991, 1), end = c(1991, nTrain))
-valid.ts <- window(ridership.ts, start = c(1991, nTrain + 1), end = c(1991, nTrain + stepsAhead))
 ridership.lm <-  tslm(train.ts ~ trend + I(trend^2))
+# Use forecast() to make prediction based on training set (train.ts)
 ridership.lm.pred <- forecast(ridership.lm, h = stepsAhead, level = 0)
 
 # Figure 3-2
@@ -22,5 +24,13 @@ text(2005.25, 2500, "Future")
 arrows(2004 - 3, 2450, 1991.25, 2450, code = 3, length = 0.1, lwd = 1,angle = 30)
 arrows(2004.5 - 3, 2450, 2004, 2450, code = 3, length = 0.1, lwd = 1,angle = 30)
 arrows(2004.5, 2450, 2006, 2450, code = 3, length = 0.1, lwd = 1, angle = 30)
+
+# Create validation set
+valid.ts <- window(ridership.ts, start = c(1991, nTrain + 1), end = c(1991, nTrain + stepsAhead))
+# Check Goodness-of-fit with accuracy() to get ME, MAE, etc.
+accuracy(ridership.lm.pred$mean, valid.ts)
+# Check prediction accuracy of validation set only
+accuracy(tail(ridership.lm.pred$fitted,36),
+         tail(valid.ts,36))
 
 
